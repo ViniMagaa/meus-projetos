a30d8194529ff9fe34a6562652bff03a62b70a33
ViniMagaa <vinisantos2008vs@gmail.com>
2023-10-18 17:35:22 -0300

    Projeto: notas "finalizado"
    Provavelmente, futuramente eu irei adicionar mais funcionalidades

--- html-css-js/notas/script.js
+++ html-css-js/notas/script.js
@@ -3,18 +3,18 @@ function createElementOnHTML(
 	innerText = "",
 	classification = "",
 	id = "",
-	bgcolor = ""
+	color = ""
 ) {
 	const element = document.createElement(tag);
 	element.innerText = innerText;
 	if (classification) element.classList.add(classification);
 	if (id) element.id = id;
-	if (bgcolor) element.style.backgroundColor = bgcolor;
+	if (color) element.style.backgroundColor = color;
 
 	return element;
 }
 
-function editNote(title, content, btnEdit) {
+function editNote(title, content, btnEdit, color, noteId) {
 	if (btnEdit.innerText === "✏️") {
 		title.contentEditable = true;
 		content.contentEditable = true;
@@ -24,22 +24,49 @@ function editNote(title, content, btnEdit) {
 		title.contentEditable = false;
 		content.contentEditable = false;
 		btnEdit.innerText = "✏️";
+
+		// Update note on local storage
+		const note = {
+			id: noteId,
+			title: title.innerText,
+			content: content.innerText,
+			color: color,
+		};
+		const newNote = JSON.stringify(note);
+
+		return localStorage.setItem(`note-${noteId}`, newNote);
 	}
 }
 
-function deleteNote(button) {
-	const note = button.closest(".note");
-	note.remove();
+function deleteNote(noteId) {
+	localStorage.removeItem(`note-${noteId}`);
+	loadNotesFromLocalStorage();
+	return;
 }
 
-function addNote(title, content, color) {
+let noteId = localStorage.getItem("noteId") || 0;
+
+function addNote(title, content, color, idNote) {
 	const lightColor = `var(--light-${color})`;
 	const mediumColor = `var(--medium-${color})`;
-
-	const notesContainer = document.getElementById("container-notes");
+	
+	const divNotesContainer = document.getElementById("container-notes");
+	const firstNote = divNotesContainer.querySelector(".note");
 	const divNote = createElementOnHTML("div", "", "note");
-	const divNoteTitle = createElementOnHTML("div", "", "note-title", "", mediumColor);
-	const divNoteContent = createElementOnHTML("div", "", "note-content", "", lightColor);
+	const divNoteTitle = createElementOnHTML(
+		"div",
+		"",
+		"note-title",
+		"",
+		mediumColor
+	);
+	const divNoteContent = createElementOnHTML(
+		"div",
+		"",
+		"note-content",
+		"",
+		lightColor
+	);
 	const h4Title = createElementOnHTML("h4", title);
 	const pContent = createElementOnHTML("p", content);
 	const divActionButtons = createElementOnHTML("div", "", "action-buttons");
@@ -58,9 +85,6 @@ function addNote(title, content, color) {
 		mediumColor
 	);
 
-	buttonEdit.onclick = () => editNote(h4Title, pContent, buttonEdit);
-	buttonDelete.onclick = () => deleteNote(buttonDelete);
-
 	divNoteTitle.appendChild(h4Title);
 	divActionButtons.appendChild(buttonEdit);
 	divActionButtons.appendChild(buttonDelete);
@@ -69,7 +93,11 @@ function addNote(title, content, color) {
 	divNote.appendChild(divNoteTitle);
 	divNote.appendChild(divNoteContent);
 
-	notesContainer.appendChild(divNote);
+	buttonEdit.onclick = () =>
+		editNote(h4Title, pContent, buttonEdit, color, idNote);
+	buttonDelete.onclick = () => deleteNote(idNote);
+
+	return divNotesContainer.insertBefore(divNote, firstNote);;
 }
 
 function checkIfExistsContent(title, content) {
@@ -99,24 +127,40 @@ function createNote() {
 	const errorContent = document.getElementById("content-error-container");
 	const noteTitle = document.getElementById("title");
 	const noteContent = document.getElementById("content");
-	
+
 	errorTitle.style.display = "none";
 	errorContent.style.display = "none";
 	noteTitle.value = "";
 	noteContent.value = "";
-	
+
 	formNewNote.style.display = "flex";
 	noteTitle.focus();
-	
+
 	const buttonCreateNote = document.getElementById("button-create-note");
 	buttonCreateNote.addEventListener("click", (e) => {
 		e.preventDefault();
 		const color = document.querySelector("input[name=color]:checked").value;
 		if (checkIfExistsContent(noteTitle.value, noteContent.value)) {
-			addNote(noteTitle.value, noteContent.value, color);
+			// Incrementar o contador para obter um ID único para a nova nota
+			noteId++;
+			localStorage.setItem("noteId", noteId);
+
+			// Add to local storage
+			const note = {
+				id: noteId,
+				title: noteTitle.value,
+				content: noteContent.value,
+				color: color,
+			};
+			localStorage.setItem(`note-${note.id}`, JSON.stringify(note));
+
+			addNote(noteTitle, noteContent, color, note.id);
+
 			noteTitle.value = "";
 			noteContent.value = "";
 			formNewNote.style.display = "none";
+
+			loadNotesFromLocalStorage();
 			return;
 		}
 	});
@@ -125,21 +169,45 @@ function createNote() {
 function closeForm() {
 	const formNewNote = document.getElementById("create-new-note");
 
-	formNewNote.style.display = 'none'
+	formNewNote.style.display = "none";
 }
 
-/* MENU */
+function loadNotesFromLocalStorage() {
+	const divNoNotes = document.getElementById("no-notes");
+	const divNotesContainer = document.getElementById("container-notes");
+
+	divNotesContainer.innerHTML = "";
+
+	if (localStorage.length > 1) {
+		divNoNotes.style.display = "none";
+		for (let i = 0; i <= localStorage.length; i++) {
+			const key = localStorage.key(i);
+
+			if (key && key.startsWith("note-")) {
+				const noteJSON = localStorage.getItem(key);
+				if (noteJSON) {
+					const note = JSON.parse(noteJSON);
+					addNote(note.title, note.content, note.color, note.id);
+				}
+			}
+		}
+	} else {
+		divNoNotes.style.display = "block";
+	}
+}
+
+// MENU
 function toggleMenu() {
 	const menu = document.getElementById("menu");
 	menu.classList.toggle("active");
 }
 
 const menuHamburger = document.getElementById("menu-hamburger");
-console.log(menuHamburger);
 menuHamburger.addEventListener("click", toggleMenu);
 
+// COMPARTILHAR
 function copyURL() {
-	const url= "https://vinimagaa.github.io/meus-projetos/html-css-js/notas/" 
+	const url = "https://vinimagaa.github.io/meus-projetos/html-css-js/notas/";
 	navigator.clipboard
 		.writeText(url)
 		.then(() => {
@@ -147,5 +215,7 @@ function copyURL() {
 		})
 		.catch(() => {
 			alert("Algo deu errado");
-		});;
+		});
 }
+
+loadNotesFromLocalStorage();
